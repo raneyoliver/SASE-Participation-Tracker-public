f5ebbdea2ef3c7a7786e22e45c8714c16d2cc41e
Davis Kirchhofer <davis_kirchhofer@tamu.edu>
2020-09-23 11:27:33 -0500

    small fixes and rubocop changes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,8 +10,9 @@ class Api::UsersController < ApplicationController
   end
 
   def create
+    # Evelio, perform a check here and make sure that the UIN isn't already in the system
     @user = User.new(user_params)
-    head :created and return if @user.save!
+    head :created and return if @user.save
 
     head :bad_request and return
   rescue StandardError
