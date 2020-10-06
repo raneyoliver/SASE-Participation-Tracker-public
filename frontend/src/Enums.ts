037a9f1268dc33338a59d834fe6aa4b704b1b2eb
Ryan Conn <rconn478@gmail.com>
2020-10-06 13:02:53 -0500

    Updated Enums * Old format slightly bothered me so I fixed it

--- frontend/src/Enums.ts
+++ frontend/src/Enums.ts
@@ -1,19 +1,14 @@
 // A Enum to restrict the value of form_type
-enum FormType {
-    'sign-in' = 'Sign-in Form',
-    'RSVP' = 'RSVP Form'
-  }
+export enum FormType {
+  'sign-in' = 'Sign-in Form',
+  'RSVP' = 'RSVP Form',
+}
 
 // A Enum to restrict the value of event_type
-enum EventType {
+export enum EventType {
   gbm = 'GBM',
   socials = 'Socials',
   mentor = 'Mentor/Mentee',
   volunteer = 'Volunteer',
   profitshare = 'Profit Share',
 }
-
-export {
-  FormType,
-  EventType,
-};
