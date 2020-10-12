5e3c8dcf8836af3c4c0be413eb7ee21b9a97817e
Ryan Conn <rconn478@gmail.com>
2020-10-12 16:35:57 -0500

    Created calendar view

---
+++ frontend/src/components/Homepage/EventView/EventCalendar/EventCalendar.tsx
@@ -0,0 +1,114 @@
+import { Box, Paper, Typography } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
+import * as React from 'react';
+import { SASEBlue } from '../../../../theme';
+import { SerializedEvent } from '../../../../types/Event';
+import { daysForMonth } from '../../../../utils/calendarUtils';
+import EventChips from './EventChips/EventChips';
+import MonthSelect from './MonthSelect/MonthSelect';
+
+interface EventCalendarProps {
+  events: SerializedEvent[];
+}
+
+const now = new Date();
+const currentYear = now.getFullYear();
+const currentMonth = now.getMonth();
+const currentDay = now.getDate();
+
+const EventCalendar: React.FC<EventCalendarProps> = ({ events: initialEvents }) => {
+  const [events, setEvents] = React.useState(initialEvents);
+
+  const [selectedYear, setSelectedYear] = React.useState(currentYear);
+  const [selectedMonth, setSelectedMonth] = React.useState(currentMonth);
+
+  const handleYearChange = (newYear: number): void => {
+    setSelectedYear(newYear);
+  };
+
+  const handleMonthChange = (newMonth: number): void => {
+    setSelectedMonth(newMonth);
+  };
+
+  // eventMap is a 3D map with year, month, and day for each event
+  // To get events for a month: eventMap.get(year).get(month).get(day)
+  const eventMap = React.useMemo(() => {
+    const ret = new Map<number, Map<number, Map<number, SerializedEvent[]>>>();
+    events.forEach((event) => {
+      const eventTime = new Date(event.start_time);
+      const eventYear = eventTime.getFullYear();
+      const eventMonth = eventTime.getMonth();
+      const eventDay = eventTime.getDate();
+
+      // Map event to its year
+      if (!ret.get(eventYear)) {
+        ret.set(eventYear, new Map<number, Map<number, SerializedEvent[]>>());
+      }
+      const eventsForYear = ret.get(eventYear);
+
+      // Map event to its month
+      if (!eventsForYear.get(eventMonth)) {
+        eventsForYear.set(eventMonth, new Map<number, SerializedEvent[]>());
+      }
+      const eventsForMonth = eventsForYear.get(eventMonth);
+
+      // Map event to its day
+      const eventsForDay = eventsForMonth.get(eventDay) || [];
+      eventsForDay.push(event);
+      eventsForMonth.set(eventDay, eventsForDay);
+    });
+
+    return ret;
+  }, [events]);
+
+  // Calendar grid items
+  const calendarDays = daysForMonth(selectedYear, selectedMonth).map((date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const isToday = day === currentDay && month === currentMonth && year === currentYear;
+    const eventsForDay = eventMap.get(year)?.get(month)?.get(day) || [];
+    const key = `${year}${month}${day}`;
+
+    // Give gray names + background for days in other months
+    const textColor = month === selectedMonth ? 'textPrimary' : 'textSecondary';
+    let backgroundColor = month === selectedMonth ? 'white' : grey[300];
+
+    // If day is the current day, highlight it on the calendar
+    if (isToday) backgroundColor = `${SASEBlue}40`;
+
+    return (
+      <Paper style={{ backgroundColor }} elevation={1} key={key}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          height="100%"
+          paddingX={0.5}
+        >
+          <Box padding={1} width="calc(min-content + 8px)">
+            <Typography variant="h4" color={textColor}>
+              {day}
+            </Typography>
+          </Box>
+          <EventChips events={eventsForDay} />
+        </Box>
+      </Paper>
+    );
+  });
+
+  return (
+    <>
+      <MonthSelect onYearChange={handleYearChange} onMonthChange={handleMonthChange} />
+      <Box
+        display="grid"
+        gridGap={8}
+        gridTemplateColumns="repeat(7, 1fr)"
+        gridAutoRows="max(150px, 15vh)"
+      >
+        {calendarDays}
+      </Box>
+    </>
+  );
+};
+
+export default EventCalendar;
