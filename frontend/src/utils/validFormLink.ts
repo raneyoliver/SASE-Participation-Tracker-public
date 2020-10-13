ee74aee95a5bb708e40b254c4e7d4d664a680ed5
Oliver Raney <oliver@pineywoodstech.com>
2020-10-12 20:58:07 -0500

    Update validFormLink.ts

--- frontend/src/utils/validFormLink.ts
+++ frontend/src/utils/validFormLink.ts
@@ -2,8 +2,14 @@ import { SerializedEvent, SerializedFormData } from '../types/Event';
 
 export default function validFormLink(event: SerializedEvent, form: SerializedFormData): string {
   const now = new Date();
-  if (now < (new Date(event.start_time)) || now > (new Date(event.end_time))) {
-    // Cannot sign in
+  const outsideEventInterval = now < (new Date(event.start_time))
+                            || now > (new Date(event.end_time));
+  const afterEventStart = now > (new Date(event.start_time));
+  const isSignIn = form.form_type.toString() === 'sign-in';
+  const isRSVP = form.form_type.toString() === 'RSVP';
+
+  if ((isSignIn && outsideEventInterval) || (isRSVP && afterEventStart)) {
+    // Cannot sign in - outside of event interval for sign-in or too late to RSVP
     return '/form/unavailable';
   }
   // Go to sign-in form ok
