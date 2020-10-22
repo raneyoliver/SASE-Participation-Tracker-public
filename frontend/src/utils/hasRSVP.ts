3b5f1cf8aa3e823085185e1d96f7780ff201d7fa
Oliver Raney <oliver@pineywoodstech.com>
2020-10-22 14:18:36 -0500

    Update hasRSVP.ts

--- frontend/src/utils/hasRSVP.ts
+++ frontend/src/utils/hasRSVP.ts
@@ -1,11 +1,5 @@
 import { SerializedEvent } from '../types/Event';
 
 export default function hasRSVP(event: SerializedEvent): boolean {
-  const formTypes = event.forms.map((form) => (
-    form.form_type.toString()
-  ));
-  if (formTypes.includes('RSVP')) {
-    return true;
-  }
-  return false;
+  return event.forms.some((form) => form.form_type.toString() === 'RSVP');
 }
