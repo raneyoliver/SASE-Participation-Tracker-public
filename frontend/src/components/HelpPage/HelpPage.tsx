e8ff82e3a207329d69913552f135736ecd45f146
Oliver Raney <oliver@pineywoodstech.com>
2020-10-30 13:25:03 -0500

    Update HelpPage.tsx

--- frontend/src/components/HelpPage/HelpPage.tsx
+++ frontend/src/components/HelpPage/HelpPage.tsx
@@ -21,36 +21,37 @@ const HelpPage: React.FC<RouteComponentProps> = () => (
                 <ListItemText
                   primary="Why can't I sign into an event form?"
                   secondary="The creator of the event may have restricted
-                   the sign-in form to only be accessible during the event time
-                   or the RSVP form to be accessible before the event time. Another
-                   reason may be that the form was deleted. Try refreshing your
-                   browser."
+                  the sign-in form to only be accessible during the event time
+                  or the RSVP form to be accessible before the event time. Another
+                  reason may be that the form was deleted. Try refreshing your
+                  browser."
                 />
               </ListItem>
               <ListItem>
                 <ListItemText
                   primary="As an administrator, how do I make changes to an event?"
                   secondary="In the top right corner of each event on the homepage,
-                   there is a dropdown box represented by three dots. Click them and select 'Edit Event'
-                   to change the event, or 'Delete Event' to delete the event altogether."
+                  there is a dropdown box represented by three dots. Click
+                  them and select 'Edit Event' to change the event, or
+                  'Delete Event' to delete the event altogether."
                 />
               </ListItem>
               <ListItem>
                 <ListItemText
                   primary="Do I need to be logged in if I'm not an administrator?"
                   secondary="No. Non-admins do not have to login, but you can still
-                   sign into events without being logged in
-                   by just typing your TAMU UIN into an event form."
+                  sign into events without being logged in
+                  by just typing your TAMU UIN into an event form."
                 />
               </ListItem>
               <ListItem>
                 <ListItemText
                   primary="How do I sign into an event if it's my first time?"
                   secondary="From the homepage, click on a form for an event,
-                   enter your UIN, and submit. If this is your first time signing in, you will
-                   automatically be redirected to the new user page. Here you
-                   can put in your info so that the system will remember you
-                   next time."
+                  enter your UIN, and submit. If this is your first time signing in, you will
+                  automatically be redirected to the new user page. Here you
+                  can put in your info so that the system will remember you
+                  next time."
                 />
               </ListItem>
             </List>
@@ -63,30 +64,30 @@ const HelpPage: React.FC<RouteComponentProps> = () => (
                 <ListItemText
                   primary="Add an RSVP form to an existing event as an administrator"
                   secondary="From the homepage, click on the three dots on the
-                   top right corner of an event card. Select 'Create RSVP Form'"
+                  top right corner of an event card. Select 'Create RSVP Form'"
                 />
               </ListItem>
               <ListItem>
                 <ListItemText
                   primary="Change time restrictions of forms as an administrator"
                   secondary="From the homepage, click on the three dots on the
-                   top right corner of an event card. Select 'Edit Event'. Then
-                   toggle which restrictions should apply at the bottom of the form."
+                  top right corner of an event card. Select 'Edit Event'. Then
+                  toggle which restrictions should apply at the bottom of the form."
                 />
               </ListItem>
               <ListItem>
                 <ListItemText
                   primary="View past events in a list view"
                   secondary="Click 'List' at the top left of the homepage and then
-                   go to 'Date Filter' and select 'All' or 'Past'. You can also use
-                   combinations of filters to see exactly what events you need."
+                  go to 'Date Filter' and select 'All' or 'Past'. You can also use
+                  combinations of filters to see exactly what events you need."
                 />
               </ListItem>
               <ListItem>
                 <ListItemText
                   primary="Quickly return to the homepage"
                   secondary="Click 'SASE-Participation Tracker' at the very top
-                   of any page to return to the homepage."
+                  of any page to return to the homepage."
                 />
               </ListItem>
             </List>
@@ -98,8 +99,8 @@ const HelpPage: React.FC<RouteComponentProps> = () => (
               <ListItem>
                 <ListItemText
                   primary="This website was created by
-                   Ryan, Davis, Oliver, Evelio, and Matthew
-                   in CSCE 431, Fall 2020, at Texas A&M University."
+                  Ryan, Davis, Oliver, Evelio, and Matthew
+                  in CSCE 431, Fall 2020, at Texas A&M University."
                 />
               </ListItem>
             </List>
